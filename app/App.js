385d4b275c3d991bee6b05c65f83c2e3158b2035
cesar avina <cesaravina@gmail.com>
2017-06-20 16:18:29 -0700

    setting up App.js

---
+++ app/App.js
@@ -0,0 +1,6 @@
+// Import reactDOM and routes
+import ReactDOM from "/react-dom";
+import routes from "./config/routes";
+
+// Render the router to the "app" div in index.html
+ReactDOM.render(routes, document.getElementById("app"));
