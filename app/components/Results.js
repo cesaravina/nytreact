7472f3fc4d8fd06ca5c97b48d5bb10fd34df49db
cesar avina <cesaravina@gmail.com>
2017-06-27 14:20:28 -0700

    created Results.js/Saved.js

---
+++ app/components/Results.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+
+class Results extends Component {
+  saveArticle(article) {
+  	API.saveArticle(article).then(this.props.getSaved);
+  }
+  render() {
+    return (
+	  <li className="list-group-item" data-date={this.props.article.pub_date}>
+		  <a href="this.props.article.web_url">
+		    {this.props.article.headline.main}
+	      </a>
+		  <button
+		    className="btn btn-default saveBtn" 
+		    onClick={() => this.saveArticle(this.props.article)}
+		  >  
+		    Save
+		  </button>    
+	  </li>
+    );
+  }
+}
+
+export default Results;
---
+++ app/components/Saved.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+
+class Saved extends Component {
+  deleteArticle(id) {
+  	API.deleteSaved(id).then(this.props.getSaved);
+  }	
+  addComment(comment) {
+  	// API.addComment(saved).then(this.props.getSaved);
+  }
+  render() { 
+    return (
+	  <li className="list-group-item" data-date={this.props.saved.articleDate}>
+		  <a href={this.props.saved.url}>
+		    {this.props.saved.title}
+	      </a>
+		  <button
+		    className="btn btn-default saveBtn" 
+		    onClick={() => this.deleteArticle(this.props.saved._id)}
+		  >  
+		    Delete
+		  </button>    
+	  </li>
+    );    
+  }
+}
+
+export default Saved;
